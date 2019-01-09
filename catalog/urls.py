72462af2952a47e0f671a2fdadf5bfc01b8e899f
Motokulman <2594501@mail.ru>
2019-01-09 18:59:25 +0300

    First version of application moved into github

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('plans/', views.PlanListView.as_view(), name='plans'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Author, CompanyUser, PrivateUser, FormFactor
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -25,3 +26,7 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class PlanListView(generic.ListView):
+    model = FormFactor
+
